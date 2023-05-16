d08194950a005a5898232b19038d70085136d395
Sven Marcus <s.marcus@outlook.de>
2023-05-16 15:49:30 +0200

    Add rules to main code file

--- gildedrose/gildedrose.py
+++ gildedrose/gildedrose.py
@@ -1,4 +1,21 @@
-class GildedRose(object):
+"""
+General Quality Rules:
+- update_quality is called once per day
+- The Quality of a regular item drops by 1 every day
+- Once the sell by date has passed, Quality degrades twice as fast
+- The Quality of an item is never negative
+
+Special Items:
+- "Aged Brie" actually increases in Quality the older it gets
+- The Quality of an item is never more than 50
+- "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+- "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
+  Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+  Quality drops to 0 after the concert
+- "Conjured" items degrade in Quality twice as fast as normal items
+"""
+
+class GildedRose:
 
     def __init__(self, items):
         self.items = items
